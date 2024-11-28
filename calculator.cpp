51a751055604d6320f85f5e6f41427f8a4d7be0f
sandesh hambarde <sandeshhambarde2004@gmail.com>
2024-11-28 18:44:36 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<" enter the value of a and b\n";
+	cin>>a;
+	cin >>b;
+	cout << "entered value of a ="<< a  << endl;
+cout << "entered value  of b ="<< b << endl;
+
+cout<<"sum ="<<(a+b)<< endl;
+cout<<"difer ="<<(a-b)<< endl;
+cout<<"multi ="<<(a*b)<< endl;
+cout<<"division ="<<(a/(double)b)<< endl;
+cout<<"modulato ="<<(a%b)<< endl;
+}
+
